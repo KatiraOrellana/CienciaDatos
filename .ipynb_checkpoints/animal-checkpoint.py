ae29f84d86bdceed2b89eb81c5309374fa050a46
KatiraOrellana <78836134+KatiraOrellana@users.noreply.github.com>
2021-02-14 23:26:40 -0600

    Funcionalidad de gato

--- .ipynb_checkpoints/animal-checkpoint.py
+++ .ipynb_checkpoints/animal-checkpoint.py
@@ -1,2 +1,17 @@
 import sys
 
+def default():
+    print('Hello')
+    
+def gato()
+    print('Meow!')
+    
+def main():
+    if sys.argv[1] == 'gato'
+        gato()
+    else
+        default()
+
+if __name__ == '__main__':
+    main()
+
