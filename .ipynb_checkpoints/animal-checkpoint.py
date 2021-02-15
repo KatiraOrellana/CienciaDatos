b15a8a3b2edb4e25e1ddcc5eaca92d4f601ad192
KatiraOrellana <78836134+KatiraOrellana@users.noreply.github.com>
2021-02-14 23:28:51 -0600

    funcionalidad de perro

--- .ipynb_checkpoints/animal-checkpoint.py
+++ .ipynb_checkpoints/animal-checkpoint.py
@@ -1,2 +1,17 @@
 import sys
 
+def default():
+    print('Hello')
+    
+def perro()
+    print('Woof!')
+    
+def main():
+     if sys.argv[1] == 'perro'
+        perro()
+    else
+        default()
+
+if __name__ == '__main__':
+    main()
+
--- animal.py
+++ animal.py
@@ -3,8 +3,14 @@ import sys
 def default():
     print('Hello')
     
+def perro()
+    print('Woof!')
+    
 def main():
-    default()
+     if sys.argv[1] == 'perro'
+        perro()
+    else
+        default()
 
 if __name__ == '__main__':
     main()
